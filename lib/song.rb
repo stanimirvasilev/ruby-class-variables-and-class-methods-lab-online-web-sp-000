eb68daa96b989fbce33027ab302b08ccf141429d
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-17 17:31:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,15 @@ class Song
    end
    
    def self.genre_count
-     @@genres
+     artist_count = {}
+       @@artists.each do |artist|
+          if artist_count [artist]
+            artist_count [artist] += 1
+          else
+            artist_count [artist] = 1
+          end
+        end
+          artist_count
      
    end
    
