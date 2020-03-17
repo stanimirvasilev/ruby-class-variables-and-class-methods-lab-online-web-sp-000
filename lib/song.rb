55ca2501f9848e4de923928ebc139daa4dd9467e
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-17 15:26:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ class Song
   attr_accessor :name, :artist, :genre
     @@count = 0
     @@artists = []
+    @@genres = []
   
   def initialize(name,artist,genre) 
     @name = name 
@@ -10,7 +11,7 @@ class Song
     @genre = genre
     @@count += 1
     @@artists << artist
-    
+    @@genres << genre
   end
   
   
